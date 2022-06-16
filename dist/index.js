1e72d367e209d6e9e568425ffaa782836627deb0
MD Sanju <mdsanju387@gmail.com>
2022-06-16 07:27:29 +0600

    form created with TypeScript (DOM)

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log("Hello TypeScript!");
+const userForm = document.querySelector(".user-form");
+const userName = document.querySelector("#name");
+const userEmail = document.querySelector("#email");
+const userCountry = document.querySelector("#country");
+const userFeedback = document.querySelector("#feedback");
+userForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const data = {
+        userName: userName.value,
+        userEmail: userEmail.value,
+        userCountry: userCountry.value,
+        userFeedback: userFeedback.value,
+    };
+    userName.value = "";
+    userEmail.value = "";
+    userCountry.value = "";
+    userFeedback.value = "";
+    console.log(data);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello TypeScript!");
+const userForm = document.querySelector(".user-form") as HTMLFormElement;
+
+const userName = document.querySelector("#name") as HTMLInputElement;
+
+const userEmail = document.querySelector("#email") as HTMLInputElement;
+
+const userCountry = document.querySelector("#country") as HTMLSelectElement;
+
+const userFeedback = document.querySelector("#feedback") as HTMLTextAreaElement;
+
+userForm.addEventListener("submit", (event: Event) => {
+  event.preventDefault();
+
+  const data = {
+    userName: userName.value,
+    userEmail: userEmail.value,
+    userCountry: userCountry.value,
+    userFeedback: userFeedback.value,
+  };
+  userName.value = "";
+  userEmail.value = "";
+  userCountry.value = "";
+  userFeedback.value = "";
+  console.log(data);
+});
